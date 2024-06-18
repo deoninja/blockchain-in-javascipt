c7b029253ec7f442a451a6a0eea3c997be243925
deo <deoninja@gmail.com>
2024-06-18 18:35:42 +0800

    Create New Block Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,19 +1,23 @@
-///////////////////////////////
-//Using function constructor//
-/////////////////////////////
 function Blockchain() {
   this.chain = [];
   this.newTransactions = [];
 }
 
-////////////////////////////
-//Using class constructor//
-//////////////////////////
-class Blockchain {
-  constructor() {
-    this.chain = [];
-    this.newTransactions = [];
-  }
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockhash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockhash: previousBlockhash,
+  };
 
-  //....
-}
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+  return newBlock;
+};
