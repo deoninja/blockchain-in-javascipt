ad9e35c3d664d81f88cb67b46b3b49dfbee301ff
deo <deoninja@gmail.com>
2024-06-18 19:29:33 +0800

    Testing Hash Block Method

--- dev/test.js
+++ dev/test.js
@@ -2,16 +2,26 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, '0IJMNBGFGFGFHG', '90FFJJLJHJJLHJHYT');
+const previousBlockhash = '0x123456789012353546546';
+const currentBlockData = [
+  {
+    amount: 10,
+    sender: '1654654655665456SDSD',
+    recipient: 'LJHLJLJNFDFYHGFKGFOU54',
+  },
+  {
+    amount: 50,
+    sender: 'FUYGJHBVLJBLIKBLGKBKJH',
+    recipient: '76943272374032784082JKHK',
+  },
+  {
+    amount: 250,
+    sender: 'KJHKJGFDGDFXFGFGFXCGFGF',
+    recipient: '8465153131GTFGFTFYFGFD',
+  },
+];
 
-bitcoin.createNewTransaction(500, '0IJKJHJHJ456456JHJH', '0IJHJHJHJHJH56H');
+const nonce = 100;
+const hash = bitcoin.hashBlock(previousBlockhash, currentBlockData, nonce);
 
-bitcoin.createNewBlock(54845, '0IJMN556621FHG', '90FFJJLJ56564658T');
-
-bitcoin.createNewTransaction(100, '0IJKJHJ98796546651', '0IJ324233242HJH56H');
-bitcoin.createNewTransaction(400, '0IJKJHJHJ456456JHJH', '0IJ5465465156256H');
-bitcoin.createNewTransaction(500, '0IJKJ54654645656JHJH', '0IJ21687465165156H');
-
-bitcoin.createNewBlock(54823445, '0I3590456621FHG', '90FFJ8787878758T');
-
-console.log(bitcoin.chain[2]);
+console.log(hash);
