2bd61258ff1bcf7d9d31561c2fd7085a169a0ee4
deo <deoninja@gmail.com>
2024-06-18 18:47:01 +0800

    Last Block Method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -22,4 +22,8 @@ Blockchain.prototype.createNewBlock = function (
   return newBlock;
 };
 
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
 module.exports = Blockchain;
