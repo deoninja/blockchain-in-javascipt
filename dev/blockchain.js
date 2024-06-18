47d062f87126e3c76afafefcfe8d9b1f3ae70daf
deo <deoninja@gmail.com>
2024-06-18 18:27:13 +0800

    Blockchain Constructor Function

---
+++ dev/blockchain.js
@@ -0,0 +1,19 @@
+///////////////////////////////
+//Using function constructor//
+/////////////////////////////
+function Blockchain() {
+  this.chain = [];
+  this.newTransactions = [];
+}
+
+////////////////////////////
+//Using class constructor//
+//////////////////////////
+class Blockchain {
+  constructor() {
+    this.chain = [];
+    this.newTransactions = [];
+  }
+
+  //....
+}
