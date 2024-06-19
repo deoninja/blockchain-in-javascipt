fb86c61c9301de032bed9795f006f4e530dc8835
deo <deoninja@gmail.com>
2024-06-19 20:41:51 +0800

    Building The GET blockchain Endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/blockchain', function (req, res) {});
+app.get('/blockchain', function (req, res) {
+  res.send(bitcoin);
+});
 
 app.post('/transaction', function (req, res) {
   console.log(req.body);
