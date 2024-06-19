b643cb46b0bf4dbcd8175860c0045c92752081ec
deo <deoninja@gmail.com>
2024-06-19 20:49:51 +0800

    Building The POST transaction Endpoints

--- dev/api.js
+++ dev/api.js
@@ -13,8 +13,12 @@ app.get('/blockchain', function (req, res) {
 });
 
 app.post('/transaction', function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} bitcoins`);
+  const blockIndex = bitcoin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  res.json({ note: `Transaction will be added in block ${blockIndex}.` });
 });
 
 app.get('/mine', function (req, res) {});
