568b8006e7ebcfcd9bf71804a0ce87b88fc7cf94
deo <deoninja@gmail.com>
2024-06-19 20:15:34 +0800

    Building The API Foundation

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Coding JavaScript!');
-});
+app.get('/blockchain', function (req, res) {});
 
-app.listen(3000);
+app.post('/transaction', function (req, res) {});
+
+app.get('/mine', function (req, res) {});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000....');
+});
