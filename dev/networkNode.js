0ccb9b28a9dc6a82f3757153973c637f2b0bcd62
deo <deoninja@gmail.com>
2024-06-21 18:47:53 +0800

    Building POST receive-new-block Endpoint

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -103,6 +103,21 @@ app.get('/mine', function (req, res) {
   });
 });
 
+app.post('/receive-new-block', function (req, res) {
+  const newBlock = req.body.newBlock;
+  const lastBlock = bitcoin.getLastBlock();
+  const correctHash = lastBlock.hash === newBlock.previousBlockHash;
+  const correctIndex = lastBlock['index'] + 1 === newBlock['index'];
+
+  if (correctHash && correctIndex) {
+    bitcoin.chain.push(newBlock);
+    bitcoin.pendingTransactions = [];
+    res.json({ note: 'New block received and accepted', newBlock: newBlock });
+  } else {
+    res.json({ note: 'New block rejected', newBlock: newBlock });
+  }
+});
+
 // //register a node and broadcast it in the whole networks
 // app.post('/register-and-broadcast-node', function (req, res) {
 //   const newNodeUrl = req.body.newNodeUrl;
